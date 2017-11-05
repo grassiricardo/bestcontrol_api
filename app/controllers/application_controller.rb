84e7bf0d17932756891e79ed9e894b88557876b6
Ricardo Grassi <grassiricardorg@gmail.com>
2017-11-05 00:05:18 -0200

    test render 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include JSONAPI::ActsAsResourceController
+
+  def not_found
+    respond_to do |format|
+      format.html { render :file => File.join(Rails.root, 'public', '404.html') }
+      format.json { render :text => '{"error": "not_found"}' }
+    end
+  end
 end
